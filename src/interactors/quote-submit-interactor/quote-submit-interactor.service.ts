1cfc73cec55a6a69423024ee39c5d71dbdf2273b
Felix <jonfelixrico@gmail.com>
2020-09-05 19:25:02 +0800

    Adjust the quote submit interactor to use the quote repository

--- src/interactors/quote-submit-interactor/quote-submit-interactor.service.ts
+++ src/interactors/quote-submit-interactor/quote-submit-interactor.service.ts
@@ -5,60 +5,53 @@ import QuoteSubmitInteractor, {
   IApproveQuoteOutput,
   IGetPendingQuotesOutput,
 } from 'src/common/classes/interactors/quote-submit-interactor.class'
-import QuoteModel from 'src/mongoose/models/quote.model'
 import InteractorError, {
   InteractorErrorCodes,
 } from 'src/common/classes/errors/interactor-error.class'
+import QuoteRepository from 'src/common/classes/repositories/quote-repository.class'
 
 @Injectable()
 export class QuoteSubmitInteractorService extends QuoteSubmitInteractor {
+  constructor(private quoteRepo: QuoteRepository) {
+    super()
+  }
+
   async submitQuote(input: ISubmitQuoteInput): Promise<ISubmitQuoteOutput> {
-    const quote = await QuoteModel.create({
-      ...input,
-      approvalStatus: {
-        ...input,
-      },
-    })
+    const quote = await this.quoteRepo.createQuote(input)
+    const approvalStatus = await this.quoteRepo.setQuoteApprovalStatus(
+      quote.quoteId,
+      input
+    )
 
     return {
-      quote: quote.baseData,
-      approvalStatus: quote.approvalStatus,
+      quote,
+      approvalStatus,
     }
   }
 
   async approveQuote(messageId: string): Promise<IApproveQuoteOutput> {
-    const quote = await QuoteModel.findOne({
-      'approvalStatus.messageId': messageId,
-    }).exec()
+    const quote = await this.quoteRepo.getQuoteByMessageId(messageId)
 
     if (!quote) {
       throw new InteractorError(InteractorErrorCodes.QUOTE_NOT_FOUND)
-    } else if (quote.approvalStatus.approveDt) {
+    }
+
+    const { quoteId } = quote
+    const approvalStatus = await this.quoteRepo.getQuoteApprovalStatus(quoteId)
+
+    if (approvalStatus.approveDt) {
       throw new InteractorError(InteractorErrorCodes.QUOTE_APPROVED)
-    } else if (new Date().getTime() > quote.approvalStatus.expireDt.getTime()) {
+    } else if (new Date().getTime() > approvalStatus.expireDt.getTime()) {
       throw new InteractorError(InteractorErrorCodes.QUOTE_EXPIRED)
     }
 
-    quote.approvalStatus.approveDt = new Date()
-    await quote.save()
+    approvalStatus.approveDt = new Date()
+    await this.quoteRepo.setQuoteApprovalStatus(quoteId, approvalStatus)
 
-    return quote.baseData
+    return quote
   }
 
   async getPendingQuotes(serverId: string): Promise<IGetPendingQuotesOutput> {
-    const quotes = await QuoteModel.find({
-      'approvalStatus.serverId': serverId,
-      'approvalStatus.expireDt': {
-        $gt: new Date(),
-      },
-      'approvalStatus.approveDt': null,
-    }).exec()
-
-    return quotes.map(q => {
-      return {
-        quote: q.baseData,
-        approvalStatus: q.approvalStatus,
-      }
-    })
+    return await this.quoteRepo.getPendingQuotes(serverId)
   }
 }
