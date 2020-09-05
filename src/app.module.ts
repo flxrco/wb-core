f0b9e83c03cf72c18edcda240dcb7580d53ddbf7
Felix <jonfelixrico@gmail.com>
2020-09-05 22:39:40 +0800

    Create module for microservice controllers

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common'
 import { ProvidersModule } from './providers/providers.module'
 import { InteractorsModule } from './interactors/interactors.module'
 import { RepositoriesModule } from './repositories/repositories.module'
+import { ControllersModule } from './controllers/controllers.module';
 
 @Module({
-  imports: [ProvidersModule, InteractorsModule, RepositoriesModule],
+  imports: [ProvidersModule, InteractorsModule, RepositoriesModule, ControllersModule],
   controllers: [],
   providers: [],
 })
