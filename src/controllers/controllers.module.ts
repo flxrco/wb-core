c7d3ad669ffd7202bd979a783c453c513789b5e9
Felix <jonfelixrico@gmail.com>
2020-09-05 22:41:28 +0800

    Push template code for the controllers

--- src/controllers/controllers.module.ts
+++ src/controllers/controllers.module.ts
@@ -1,4 +1,8 @@
 import { Module } from '@nestjs/common';
+import { QuoteSubmitController } from './quote-submit/quote-submit.controller';
+import { QuoteReceiveController } from './quote-receive/quote-receive.controller';
 
-@Module({})
+@Module({
+  controllers: [QuoteSubmitController, QuoteReceiveController]
+})
 export class ControllersModule {}
