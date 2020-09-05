bc8a7671cea6da6dd3a46968ff577826d580ea6b
Felix <jonfelixrico@gmail.com>
2020-09-05 22:05:22 +0800

    Remove the debug HTTP module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common'
 import { ProvidersModule } from './providers/providers.module'
-import { InteractorsModule } from './interactors/interactors.module';
-import { HttpDebugModule } from './http-debug/http-debug.module';
-import { RepositoriesModule } from './repositories/repositories.module';
+import { InteractorsModule } from './interactors/interactors.module'
+import { RepositoriesModule } from './repositories/repositories.module'
 
 @Module({
-  imports: [ProvidersModule, InteractorsModule, HttpDebugModule, RepositoriesModule],
+  imports: [ProvidersModule, InteractorsModule, RepositoriesModule],
   controllers: [],
   providers: [],
 })
