b9c57b19ef1f109dbb65cae44dbad2823bda64ff
Felix <jonfelixrico@gmail.com>
2020-09-05 22:39:24 +0800

    Prepare for microservices implementation

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,18 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+import { MicroserviceOptions, Transport } from '@nestjs/microservices'
+import 'dotenv-defaults/config'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    logger: ['debug', 'error', 'log', 'verbose', 'warn'],
-  })
-  await app.listen(3000)
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.REDIS,
+      options: {
+        url: process.env.MQ_URI,
+      },
+    }
+  )
+  app.listen(() => console.log('Microservice is listening'))
 }
 bootstrap()
