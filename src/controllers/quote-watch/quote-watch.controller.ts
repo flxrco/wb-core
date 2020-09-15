1696f842288d6a65b99b98c4b4c2a78d17759b38
Felix <jonfelixrico@gmail.com>
2020-09-15 22:06:35 +0800

    Implement microservice controller for FLAG_AS_LOST

--- src/controllers/quote-watch/quote-watch.controller.ts
+++ src/controllers/quote-watch/quote-watch.controller.ts
@@ -1,18 +1,30 @@
 import { Controller } from '@nestjs/common'
 import { MessagePattern, Payload } from '@nestjs/microservices'
+import {
+  IPendingQuote,
+  QuoteWatchInteractor,
+} from 'src/common/classes/interactors/quote-watch-interactor.class'
 import MicroserviceMessages from 'src/common/enums/microservice-messages.enum'
 import { QuoteWatchInteractorService } from 'src/interactors/quote-watch-interactor/quote-watch-interactor.service'
 
 @Controller()
-export class QuoteWatchController {
-  constructor(private interactor: QuoteWatchInteractorService) {}
+export class QuoteWatchController extends QuoteWatchInteractor {
+  constructor(private interactor: QuoteWatchInteractorService) {
+    super()
+  }
+
   @MessagePattern(MicroserviceMessages.GET_PENDING_QUOTES)
   async getPendingQuotes(@Payload() serverId: string) {
     return await this.interactor.getPendingQuotes(serverId)
   }
 
   @MessagePattern(MicroserviceMessages.APPROVE_QUOTE)
-  async approveQuote(@Payload() messageId: string) {
+  async approveByMessageId(@Payload() messageId: string) {
     return await this.interactor.approveByMessageId(messageId)
+  }
+
+  @MessagePattern(MicroserviceMessages.FLAG_AS_LOST)
+  async flagAsLost(messageId: string): Promise<IPendingQuote> {
+    return await this.interactor.flagAsLost(messageId)
   }
 }
