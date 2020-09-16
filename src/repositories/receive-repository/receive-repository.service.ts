4ff5ea27221c0a54509ed39d97c2ffa3caa4f315
Felix <jonfelixrico@gmail.com>
2020-09-16 20:03:38 +0800

    Apply corrections to the receive repository

--- src/repositories/receive-repository/receive-repository.service.ts
+++ src/repositories/receive-repository/receive-repository.service.ts
@@ -2,13 +2,18 @@ import { Injectable } from '@nestjs/common'
 import ReceiveRepository from 'src/common/classes/repositories/receive-repository.class'
 import IReceive from 'src/common/interfaces/models/receive.interface'
 import QuoteModel from 'src/mongoose/models/quote.model'
+import shortid = require('shortid')
 
 @Injectable()
 export class ReceiveRepositoryService extends ReceiveRepository {
   async createReceive(quoteId: string, receive: IReceive) {
-    const quote = await QuoteModel.findById(quoteId)
+    const quote = await QuoteModel.findOne({ quoteId })
 
-    const newReceive = quote.receives.create(receive)
+    const newReceive = quote.receives.create({
+      ...receive,
+      receiveId: shortid(),
+    })
+
     quote.receives.push(newReceive)
 
     await quote.save()
